c58b2ea99366dba9234f9f778a643f11ad99bfef
Patrik Sjöstrand <patrik.sjostrand@student.kyh.se>
2020-09-24 02:41:24 +0200

    no message

--- src/com/company/NewMain.java
+++ src/com/company/NewMain.java
@@ -5,10 +5,13 @@ public class NewMain {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             String outputString = "";
-            if (i % 5 == 0) outputString = "Buzz";
-            if (i % 3 == 0) outputString = "Fizz" + outputString.toLowerCase();
-            if (i == 42) outputString = "Liftarens guide...";
-            if (outputString.equals("")) outputString = String.valueOf(i);
+            if (i == 42) outputString = "Answer to the Ultimate Question of Life, the Universe, and Everything";
+            else {
+                if (i % 5 == 0) outputString = "Buzz";
+                if (i % 3 == 0) outputString = "Fizz" + outputString.toLowerCase();
+                if (outputString.equals("")) outputString = String.valueOf(i);
+            }
+
             System.out.println(outputString);
         }
     }
